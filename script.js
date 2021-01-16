33f2f25cfe81d95a7c6677c7288be7b869f5cadb
jounob <obeiwiljephnie@gmail.com>
2021-01-15 22:39:42 -0300

    The snake project without modification

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-window.onload = () => {
-    let canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "3px solid #ff5400";
-    document.body.appendChild(canvas);
+import {update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection} from './snake.js';
+import {update as updateFood, draw as drawFood} from './food.js';
+import {outsideGrid} from './grid.js';
 
-    let contex = canvas.getContext('2d');
-    contex.fillStyle = "#9e0059"
-    contex.fillRect(30, 30, 100, 50)
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board")
+
+function main(currentTime){
+    if(gameOver){
+        if(confirm("You lost. Press ok to restart")){
+            window.location = '/';
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+   
+    if(secondsSinceLastRender < 1 / snakeSpeed) return; 
+    
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+    
+}
+window.requestAnimationFrame(main); 
+
+function update(){
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+
+function draw(){
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
